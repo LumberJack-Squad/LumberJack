0326cc98b4044e9e69216c263b631e92cb0a6b2a
rooster6050 <edward.hawksworth@outlook.com>
2018-04-17 16:52:45 +0100

    Lead squad member, bill
    Just a skeleton implementation which will need to be replaced with data from our APIs/Backend, need to think more about Bill implementation

--- src/main/Squad.java
+++ src/main/Squad.java
@@ -5,10 +5,42 @@ import java.util.ArrayList;
 public class Squad {
 	
 	private ArrayList<User> squadMembers = new ArrayList<>();
+	private User leadUser;
+	private Bill bill;
+	
+	public Squad(double billSize)
+	{
+		bill = new Bill(billSize);
+	}
 	
 	public void addSquadMember(User newMember)
 	{
 		squadMembers.add(newMember);
+	}
+	
+	public boolean setLeadUser(User newLead)
+	{
+		if(squadMembers.contains(newLead))
+		{
+			leadUser = newLead;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public User getLeadUser()
+	{
+		if(leadUser!=null)
+		{
+			return leadUser;
+		}
+		else 
+		{
+			return null;
+		}
 	}
 	
 	public boolean removeSquadMember(User removeMember)
@@ -22,6 +54,14 @@ public class Squad {
 		{
 			return false;
 		}
+	}
+
+	public Bill getBill() {
+		return bill;
+	}
+
+	public void setBill(Bill bill) {
+		this.bill = bill;
 	}
 
 }
